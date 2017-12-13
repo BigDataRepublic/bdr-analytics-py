814c67956326044fbfe06a2c40f93904946c77b7
arbackus <a.r.backus@gmail.com>
2017-12-13 16:21:49 +0100

    test for stratefied generator in combination with keras ImageDataGenerator

--- bdranalytics/keras/tests/test_generators.py
+++ bdranalytics/keras/tests/test_generators.py
@@ -3,12 +3,14 @@ from bdranalytics.keras.generators import *
 import numpy as np
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.preprocessing.image import ImageDataGenerator
 
 
 @pytest.yield_fixture(scope='class')
 def params(request):
     request.cls.batch_size = 3
     request.cls.X = np.random.rand(100, 10)
+    request.cls.X_image = np.random.rand(100, 1, 1, 1)
     request.cls.y = 1. * (np.random.rand(100, 1) > 0.5)
     request.cls.strata_weights = {1.0: 0.2, 0.0: 0.8}
     yield
@@ -45,3 +47,19 @@ class TestGenerators:
         model.add(Dense(units=1, input_shape=self.X.shape[1:]))
         model.compile(loss='mean_squared_error', optimizer='sgd')
         model.fit_generator(iterator, steps_per_epoch=(len(self.X)/self.batch_size))
+
+    def test_stratified_image_data_generator(self):
+
+        iterator = ImageDataGenerator().flow(self.X_image, self.y)
+
+        iterator.index_generator = StratifiedIndexGenerator().flow(batch_size=self.batch_size,
+                                                                   strata=self.y,
+                                                                   strata_weights=self.strata_weights)
+        total, positives = 0, 0
+
+        for i in range(100):
+            X, y = next(iterator)
+            positives += y.sum()
+            total += len(y)
+
+        np.testing.assert_almost_equal(np.array([positives / total]), np.array([0.2]))
